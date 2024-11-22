13cbdb35b564c0d65cf5ac0ea87af291073c2d22
Max Nowak <max.nowak.j@gmail.com>
2024-11-21 17:18:56 -0800

    add Duper

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ lean_lib LeanBoogie where
   -- ## for 4.12:
   require qpf from git "https://github.com/alexkeizer/QpfTypes.git" @ "ccb042d5260e6af8f5e81fa33db83ebf0c4e093f" -- for Lean 4.12
   require auto from git "https://github.com/leanprover-community/lean-auto.git" @ "680d6d58ce2bb65d15e5711d93111b2e5b22cb1a" -- 4.12
-  -- require Duper from git "https://github.com/leanprover-community/duper.git" @ "v0.0.17"
+  require Duper from git "https://github.com/leanprover-community/duper.git" @ "25c3ea88da2505158998eea07f40b07c0cdfe5ba"
 
   -- ## for 4.11 (lean-auto only supports 4.11 for now):
   -- require auto from git "https://github.com/leanprover-community/lean-auto.git" @ "60e546ca7a9d40d508e58847a9d0630406835178" -- 4.11, breaks duper, but parses z3 output.
