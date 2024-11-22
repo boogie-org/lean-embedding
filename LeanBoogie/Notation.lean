8d296436fbe4ccffae8a0ee856b585f9036af177
Max Nowak <max.nowak.j@gmail.com>
2024-11-21 17:19:12 -0800

    Theta notation

--- LeanBoogie/Notation.lean
+++ LeanBoogie/Notation.lean
@@ -8,3 +8,12 @@ infixl:20 ";; " => Seqi.seqi
 class Cat (K : Type -> Type -> Type) where
   cat : K A B -> K B C -> K A C
 infixl:75 " >>> " => Cat.cat
+
+/-- Monad morphism from a computation monad `M` to a specification monad `W`. -/
+class Theta (M : Type -> Type) (W : outParam (Type -> Type)) where
+  θ : M A -> W A
+export Theta (θ)
+
+class LawfulTheta (M : (Type -> Type)) [m : Monad M] (W : outParam (Type -> Type)) [w : Monad W] [Theta M W] : Prop where
+  θ_pure : θ (m.pure a) = w.pure a
+  θ_bind : θ (m.bind a b) = w.bind (θ a) (fun a => θ (b a))
