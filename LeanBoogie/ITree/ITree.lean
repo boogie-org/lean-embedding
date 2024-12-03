e3a44f9ab5602d70d12271701b5c225da5476f91
Max Nowak <max.nowak.j@gmail.com>
2024-12-03 14:05:52 -0800

    `ITree (E : Type -> Type)`
    We used a workaround before, only having `ITree (E : Type)` due to a limitation in QPFTypes. We now have full ITrees.

--- LeanBoogie/ITree/ITree.lean
+++ LeanBoogie/ITree/ITree.lean
@@ -9,233 +9,216 @@ semantics of side-effecting, possibly non-terminating, programs
 
 [1] https://arxiv.org/abs/1906.00046
 [2] https://github.com/DeepSpec/InteractionTrees
+
+Thank you to Alex Keizer (github.com/alexkeizer) for the help in figuring this out!
 -/
-
-namespace Internal
-
-/-
-## Hacks / Workarounds
-
-We'd like to define interaction trees as follows:
-```
-codata ITree (ε : Type → Type) ρ where
-  | ret (r : ρ)
-  | tau (t : ITree ε ρ)
-  | vis {α : Type} (e : ε α) (k : α → ITree ε ρ)
-```
-Unfortunately, `vis` in that definition is a dependent arrow in disguise,
-and dependent arrows are currently not supported by the framework yet.
-
-What is supported, though, are sigma types, as in the following, equivalent,
-definition
-```
-codata ITree (ε : Type → Type) ρ where
-  | ret (r : ρ)
-  | tau (t : ITree ε ρ)
-  | vis (e : Σ α : Type, ε α × α → ITree ε ρ)
-```
-Unfortunately, this, too yields an error, so for now we settle for fixing a
-particular input type `α`, by making `α` a parameter of the type.
--/
-codata ITree (α : Type) ε ρ where -- TODO: change ε to be Type -> Type
-| ret (r : ρ)
-| tau (t : ITree α ε ρ)
-| vis : ε → (α → ITree α ε ρ) → ITree α ε ρ
-
-end Internal
-
-/-- For now, due to a limitation in QPF, we hard code the answer type to one specific type. -/
-abbrev Ans : Type := Int
-
-def ITree E A := Internal.ITree Ans E A
 
 namespace ITree
 
--- QPF gotcha: The parameters to ITree get reordered in the ctors somehow
-def ret {E A} (r : A) : ITree E A := @Internal.ITree.ret A Ans E r
-def tau {E A} (t : ITree E A) : ITree E A := @Internal.ITree.tau Ans E A t
-def vis {E A} (e : E) (k : Ans -> ITree E A) := @Internal.ITree.vis E Ans A e k
+/-
+  ## Step 1: Defining Shape
+-/
 
-abbrev Base E A β := Internal.ITree.Base Ans E A β
-abbrev Base.Uncurried : TypeFun 3 := Internal.ITree.Base.Uncurried Ans
+-- Intuitively: `ρ` is `ITree E A`, and `ν` is `(Ans : Type) × (E Ans) × (Ans → ITree E A)`.
+inductive Shape (E : Type -> Type) (A : Type 1) (ρ : Type 1) (ν : Type 1) : Type 1
+| ret (r : A) : Shape E A ρ ν
+| tau (t : ρ) : Shape E A ρ ν
+| vis (e : ν) : Shape E A ρ ν
 
-def corec {E A β : Type} (f : β → ITree.Base E A β) (b : β) : ITree E A
-  := MvQPF.Cofix.corec (n := 2) (α := (Vec.reverse (Vec.nil.append1 A ::: E))) (F := TypeFun.ofCurried (n := 3) (ITree.Base)) f b
+abbrev Shape.Uncurried (E : Type -> Type) : TypeFun 3 := TypeFun.ofCurried (Shape E)
 
-def dest {E A : Type} : ITree E A -> ITree.Base E A (ITree E A)
-  := MvQPF.Cofix.dest
-
-/-- Just a convenience function. Re-plays a tree within another tree. -/
-def Base.replay (ta : ITree E A₁) (fTree : ITree E A₁ -> C) (fRet : A₁ -> A₂ := by exact id) : ITree.Base E A₂ C :=
-  match ta.dest with
-  | .ret (a : A₁) => .ret (fRet a)
-  | .tau (t : ITree E A₁) => .tau (fTree t)
-  | .vis e k => .vis e (fun x => fTree (k x))
-
-def _root_.TypeVec.ofList : (l : List Type) -> TypeVec l.length
-| [] => Vec.nil
-| t :: l => TypeVec.ofList l |>.append1 t
-
-def Base.Map (f : C -> D) : TypeVec.Arrow (TypeVec.ofList [C, B, E]) (TypeVec.ofList [D, B, E])
-  := TypeVec.appendFun TypeVec.id f
-
-def Base.Inr : TypeVec.Arrow (TypeVec.ofList [ITree E B, B, E]) (TypeVec.ofList [ITree E A ⊕ ITree E B, B, E])
-  := TypeVec.appendFun TypeVec.id Sum.inr
-
-@[cases_eliminator, elab_as_elim]
-def cases {E A : Type} {motive : ITree E A → Sort u}
-    (ret : (r : A) → motive (.ret r))
-    (tau : (x : ITree E A) → motive (.tau x))
-    (vis : (e : E) → (k : Ans → ITree E A) → motive (.vis e k)) :
-    ∀ (x : ITree E A), motive x :=
-  fun x =>
-    match h : MvQPF.Cofix.dest x with
-    | .ret r =>
-      have h : x = .ret r := by
-        apply_fun MvQPF.Cofix.mk at h
-        simpa [MvQPF.Cofix.mk_dest] using h
-      h ▸ ret r
-    | .tau y =>
-      have h : x = .tau y := by
-        apply_fun MvQPF.Cofix.mk at h
-        simpa [MvQPF.Cofix.mk_dest] using h
-      h ▸ tau y
-    | .vis e k =>
-      have h : x = .vis e k := by
-        apply_fun MvQPF.Cofix.mk at h
-        simpa [MvQPF.Cofix.mk_dest] using h
-      h ▸ vis e k
-
--- Computation rules
-theorem cases_ret : cases (motive := motive) c_ret c_tau c_vis (.ret r) = c_ret r := rfl
-theorem cases_tau : cases (motive := motive) c_ret c_tau c_vis (.tau t) = c_tau t := sorry
-theorem cases_vis : cases (motive := motive) c_ret c_tau c_vis (.vis e k) = c_vis e k := sorry
-
-def spin : ITree E A := corec (fun () => .tau ()) ()
-
-/-- Execute a finite amount of steps of a potentially infinite
-  Returns the events encountered along the way (if any), and the final state (if any).
-  Uses `f` to determine the answer to events. -/
-def run (t : ITree E A) (f : E -> Ans) : Nat -> (List E) × Option A
-| 0 => ([], none)
-| n+1 => match t.dest with
-  | .ret a => ([], some a)
-  | .tau t => run t f n
-  | .vis e k => by
-    let t : ITree E A := k (f e)
-    let (evs, ret) := run t f n
-    exact (e :: evs, ret)
-
-/-- We use theorems from the ITree paper without proof, for now. Give a justification or source in `ref`. -/
-axiom trustITree {P : Prop} (ref : String) : P
+instance : MvFunctor (Shape.Uncurried E) where
+  map f
+    | .ret a => .ret (f 2 a)
+    | .tau t => .tau (f 1 t)
+    | .vis e => .vis (f 0 e)
 
 /-
-  ## `ITree` forms a monad
+  ## Step 2: Functors for constructors
 -/
 
-def pure (a : A) : ITree E A := .ret a
+qpf G_ret (E : Type -> Type) A ρ := A
+qpf G_tau (E : Type -> Type) A ρ := ρ
+-- qpf G_vis (E : Type -> Type) A ρ := (Ans : Type) × E Ans × (Ans → ρ) -- this unfortunately doesn't work, hence the workaround below
 
-def bind (ta : ITree E A) (tb : A -> ITree E B) : ITree E B :=
-  ITree.corec (β := Sum (ITree E A) (ITree E B)) (fun x =>
-    match x with
-    | .inl ta =>
-      match ta.dest with
-      | .ret (a : A) =>
-        let tb : ITree E B := tb a
-        let ret : ITree.Base E B (ITree E B) := tb.dest
-        let ret : ITree.Base E B (ITree E A ⊕ ITree E B) := MvFunctor.map (F := TypeFun.ofCurried (n := 3) ITree.Base) Base.Inr ret
-        ret
-      | .tau t => .tau (.inl t)
-      | .vis e k => .vis e (fun x => .inl (k x))
-    | .inr b => Base.replay b Sum.inr
-  ) (Sum.inl ta)
+section SigmaWorkaround
+  /-- `qpf G (Ans : Type) (E : Type → Type) A ρ ν := E Ans × (Ans → ρ)`, but universe-polymorphic -/
+  abbrev G.Uncurried (Ans : Type 0) (E : Type 0 → Type 0) : TypeFun.{1, 1} 2 :=
+    MvQPF.Comp (n := 2) (m := 2) -- compose two 2-ary (A, ρ) functors `E Ans` and `Ans -> ρ`
+      (TypeFun.ofCurried Prod.{1, 1})  -- ✓ MvQPF
+      ![
+        MvQPF.Const 2 (ULift (E Ans)), -- ✓ inst₁
+        MvQPF.Comp (n := 1) (m := 2) -- ✓ inst₂
+          (TypeFun.ofCurried (MvQPF.Arrow (ULift.{1} Ans)))
+          ![MvQPF.Prj (n := 2) 0]
+      ]
 
-instance : Monad (ITree E) where
-  pure := pure
-  bind := bind
+  instance inst₁ {Ans : Type} {E : Type -> Type} : MvQPF.{1, 1} (MvQPF.Const 2 (ULift (E Ans))) := inferInstance
 
-instance : LawfulFunctor (ITree E) where
-  map_const := sorry
-  id_map := sorry
-  comp_map := sorry
+  instance : MvQPF.{1,1} (MvQPF.Prj (n := 2) 0) := inferInstance
+  instance : MvQPF.{1,1} (![MvQPF.Prj (n := 2) 0] 0) := MvQPF.Prj.mvqpf 0
+  instance : ∀i, MvQPF.{1,1} (![MvQPF.Prj (n := 2) 0] i) := fun | 0 => inferInstance
+  instance inst₂ {Ans : Type} : MvQPF.{1, 1} (MvQPF.Comp (n := 1) (m := 2)
+      (TypeFun.ofCurried (MvQPF.Arrow (ULift.{1} Ans)))
+      ![MvQPF.Prj (n := 2) 0]
+    ) := inferInstance
 
-instance : LawfulMonad (ITree E) where
-  seqLeft_eq := sorry
-  seqRight_eq := sorry
-  pure_seq := sorry
-  bind_pure_comp := sorry
-  bind_map := sorry
-  pure_bind := sorry
-  bind_assoc := sorry
+  abbrev G_vis.Uncurried (E : Type → Type) : TypeFun.{1,1} 2 :=
+    MvQPF.Sigma.{1} fun (Ans : Type) => (G.Uncurried Ans E)
 
+  abbrev G_vis (E : Type → Type) (A : Type) (ρ : Type 1) : Type 1 := G_vis.Uncurried E ![ULift A, ρ]
+
+  #synth MvQPF (TypeFun.ofCurried (n := 2) @Prod.{1}) -- :)
+
+  instance inst₃ {Ans} {E : Type -> Type} : ∀i, MvQPF.{1, 1} (
+      ![
+        MvQPF.Const 2 (ULift (E Ans)), -- ✓ inst₁
+        MvQPF.Comp (n := 1) (m := 2) -- ✓ inst₂
+          (TypeFun.ofCurried (MvQPF.Arrow (ULift.{1} Ans)))
+          ![MvQPF.Prj (n := 2) 0]
+      ] i
+  ) := fun
+    | 0 => inst₁
+    | 1 => inst₂
+
+  instance {Ans} {E : Type -> Type} : MvQPF.{1, 1} (G.Uncurried Ans E) := inferInstance
+
+  #synth MvQPF.{1, 1} (G.Uncurried ?Ans ?E) -- :)
+  #synth MvQPF.{1, 1} (G_vis.Uncurried ?E) -- :)
+end SigmaWorkaround
+
+abbrev ConstructorFs (E : Type -> Type) : Fin2 3 -> TypeVec 2 -> Type 1 :=
+  ![G_ret.Uncurried E, G_tau.Uncurried E, G_vis.Uncurried E]
+
+/- For some reason tc synthesis can't figure these out by itself
+  `#synth MvQPF (G_vis.Uncurried ?E)` -- works
+  `#synth MvQPF (ConstructorFs ?E 0)` -- fails :(
+-/
+instance [inst : MvQPF (G_vis.Uncurried E)] : MvQPF (ConstructorFs E 0) := inst
+instance [inst : MvQPF (G_tau.Uncurried E)] : MvQPF (ConstructorFs E 1) := inst
+instance [inst : MvQPF (G_ret.Uncurried E)] : MvQPF (ConstructorFs E 2) := inst
+instance {E : Type -> Type} : (i : Fin2 3) -> MvQPF (ConstructorFs E i) :=
+  fun
+  | 0 => inferInstance
+  | 1 => inferInstance
+  | 2 => inferInstance
+
+/- ## Step 3: Base
+  This step takes `Shape E : (A : Type) -> (ρ : Type 1) -> (ν : Type 1) -> Type 1`,
+  and makes the `ν` disappear by expressing it in terms of `ρ`, thus obtaining
+  `Base E : (A : Type) -> (ρ : Type 1) -> Type 1`.
+-/
+
+abbrev Base.Uncurried  (E : Type -> Type) : TypeFun 2 :=
+  MvQPF.Comp
+    (TypeFun.ofCurried (n := 3) (Shape E))
+    ![G_ret.Uncurried E, G_tau.Uncurried E, G_vis.Uncurried E]
+
+abbrev Base (E : Type -> Type) (A : Type) (ρ : Type 1) : Type 1 := Base.Uncurried E ![ULift A, ρ]
+
+instance {E : Type -> Type} : MvFunctor (Base.Uncurried E) where
+  map f x := MvQPF.Comp.map f x
 
 /-
-  ## Iter
+  ## Step 4: Showing that `Base` is a QPF
+  We do this by showing that `Shape` is isomorphic to a polynomial functor defined
+  via `HeadT` and `ChildT`. Then we obtain the `MvQPF` instance for `Shape` using `ofPolynomial`.
+
+  Once we know that `Shape` is a MvQPF, we automatically know that `Base` is a MvQPF, because
+  `Base` is expressed using only MvQPFs and combinators (`Comp`, `Prj`, `Sigma`, `Arrow`, `Const`)
+  which preserve MvQPF.
 -/
 
-/-- Repeat a computation until it returns `B`.
+inductive HeadT : Type 1
+| ret
+| tau
+| vis
 
-  From the ITrees paper, page 12:
-  ```
-  CoFixpoint iter (body : A → itree E (A + B)) : A → itree E B :=
-    fun a ⇒ ab <- body a ;;
-      match ab with
-      | inl a ⇒ Tau (iter body a)
-      | inr b ⇒ Ret b
-      end.
-  ``` -/
-def iter (body : A -> ITree E (A ⊕ B)) (a₀ : A) : ITree E B := sorry
-  -- ITree.corec (fun (x : A ⊕ ITree E (B)) =>
-  --   match x with
-  --   | .inl a =>
-  --     -- Run the body, if it returned `a` we iter again, if it returned `b` we are done.
-  --     let res : ITree E (A ⊕ B) := bind (body a) (fun ab =>
-  --       match ab with
-  --       | .inl a => .ret (.inl a)
-  --       | .inr b => .ret (.inr b)
-  --     )
-  --     match res.dest with
-  --     -- | .ret (a : A ⊕ B) => .ret sorry
-  --     | .ret (.inl a) => .tau (.inl a) -- call `iter body a`
-  --     | .ret (.inr b) => .ret b -- we are done
-  --     | .tau (t : ITree E _) => .tau (.inr t)
-  --     | .vis e k => sorry
-  --   | .inr b => Base.replay b .inr
-  -- ) (Sum.inl a₀)
+abbrev ChildT : HeadT -> TypeVec.{1} 3
+| .ret => ![PFin2 1, PFin2 0, PFin2 0] -- One `A`, zero `ρ`, zero `ν`
+| .tau => ![PFin2 0, PFin2 1, PFin2 0] -- Zero `A`, one `ρ`, zero `ν` (remember, `ρ` intuitively means `ITree E A`)
+| .vis => ![PFin2 0, PFin2 0, PFin2 1] -- Zero `A`, zero ρ, one ν (where ν is our `(Ans : Type) × ...`)
 
+abbrev P : MvPFunctor.{1} 3 := ⟨HeadT, ChildT⟩
+abbrev F (E : Type -> Type) : TypeVec.{1} 3 -> Type 1 := Shape.Uncurried E
 
-/--
-  Definition loop (body : C + A → itree E (C + B)) : A → itree E B :=
-    fun a ⇒ iter (fun ca ⇒
-      cb <- body ca ;;
-      match cb with
-      | inl c ⇒ Ret (inl (inl c))
-      | inr b ⇒ Ret (inr b)
-      end) (inr a).
+def box (E : Type -> Type) : F E α → P.Obj α
+| .ret (a : α 2) => Sigma.mk HeadT.ret fun -- `a : A`
+  | 2 => fun (_ : PFin2 1) => a
+  | 1 => PFin2.elim0
+  | 0 => PFin2.elim0
+| .tau (t : α 1) => Sigma.mk HeadT.tau fun -- `t : ρ`
+  | 2 => PFin2.elim0
+  | 1 => fun (_ : PFin2 1) => t
+  | 0 => PFin2.elim0
+| .vis (e : α 0) => Sigma.mk HeadT.vis fun -- `e : ν`
+  | 2 => PFin2.elim0
+  | 1 => PFin2.elim0
+  | 0 => fun (_ : PFin2 1) => e
+
+def unbox (E : Type -> Type) : P.Obj α → F E α
+| ⟨.ret, child⟩ => Shape.ret (child 2 .fz)
+| ⟨.tau, child⟩ => Shape.tau (child 1 .fz)
+| ⟨.vis, child⟩ => Shape.vis (child 0 .fz)
+
+theorem box_unbox_id (x : P.Obj α) : box E (unbox E x) = x := by
+  rcases x with ⟨head, child⟩
+  cases head <;> (
+    rw [unbox, box]
+    congr
+    (funext i; fin_cases i) <;> (simp only [Nat.reduceAdd, Function.Embedding.coeFn_mk]; funext j; fin_cases j)
+    rfl
+  )
+
+theorem unbox_box_id (x : F E α) : unbox E (box E x) = x := by cases x <;> rfl
+
+instance Shape.instMvQPF : MvQPF (F E) := MvQPF.ofPolynomial P (box E) (unbox E) box_unbox_id unbox_box_id (by
+  intro α β f x
+  cases x <;> (simp only [Nat.reduceAdd]; rfl)
+)
+
+instance Base.instMvQPF : MvQPF (Base.Uncurried E) := inferInstance
+
+/-
+  ## Step 5: Finally `Cofix`, constructors, corecursor
+  This step takes `Base E : (A : Type) -> (ρ : Type 1) -> Type 1`, and makes the `ρ` disappear
+  by taking the cofixpoint, thus obtaining `ITree E : (A : Type) -> Type 1`.
+  We need to show that Base is a MvQPF in order to take the fixpoint, which we've shown in step 4.
+
+  And now we can:
+  - Define our constructors `ret`, `tau`, and `vis`
+  - Define our (co-)eliminator, `cases`, etc.
 -/
-def loop (body : Sum C A -> ITree E (Sum C B)) (a : A) : ITree E B := sorry
 
-def iter_lift (body : A -> ITree E (A ⊕ B)) : (A ⊕ B) -> ITree E (A ⊕ B) :=
-  fun | .inl a => body a | .inr b => return .inr b
+abbrev Uncurried (E : Type -> Type) := MvQPF.Cofix (Base.Uncurried E)
+abbrev _root_.ITree (E : Type -> Type) (A : Type) : Type 1 := Uncurried E ![ULift A]
 
+abbrev ret {E : Type -> Type} {A : Type} (a : A) : ITree E A := MvQPF.Cofix.mk (Shape.ret (.up a))
+abbrev tau {E : Type -> Type} {A : Type} (t : ITree E A) : ITree E A := MvQPF.Cofix.mk (Shape.tau t)
+abbrev vis {E : Type -> Type} {A : Type} {Ans : Type} (e : E Ans) (k : Ans -> ITree E A) : ITree E A :=
+  MvQPF.Cofix.mk (Shape.vis ⟨Ans, .up e, fun ans => k ans.down⟩)
 
--- # Experimentation:
+def corec {E : Type -> Type} {A : Type} {β : Type 1} (f : β → Base E A β) (b : β) : ITree E A
+  := MvQPF.Cofix.corec (n := 1) (F := Base.Uncurried E) f b
 
-namespace WithSigma
-  inductive ITree2.Shape ρ ι ν
-  | ret (r : ρ)
-  | tau (t : ι) -- ι = ITree ε ρ
-  | vis (e : ν) -- ν = Σ α : Type, ε α × α → ITree ε ρ
-
-  -- qpf F ε ρ ι ν    := (Σ α : Type, ε α × α → ι)
-  -- qpf F (α : Type) ε ρ ι ν    := Sigma G
-
-  qpf G (α : Type) (ε : Type → Type) ρ ι ν := ε α × (α → ι)
-
-  #check G
-  #check fun α ε => TypeFun.ofCurried (n := 3) (G α ε)
-  #check MvQPF.Sigma
-
-  -- def Gs (ε : Type 0 → Type 0) : Type 0 :=
-  --   MvQPF.Sigma (A := Type 0) (fun (α : Type _) => TypeFun.ofCurried (n:=3) (G α ε))
-end WithSigma
+def cases {E : Type -> Type} {A : Type} {motive : ITree E A -> Sort u}
+  (ret : (r : A) → motive (ret r))
+  (tau : (x : ITree E A) → motive (tau x))
+  (vis : {Ans : Type} -> (e : E Ans) → (k : Ans → ITree E A) → motive (vis e k))
+  (x : ITree E A) : motive x :=
+  match h : MvQPF.Cofix.dest x with
+  | .ret (.up r) =>
+    have h : x = ITree.ret r := by
+      apply_fun MvQPF.Cofix.mk at h
+      simpa [MvQPF.Cofix.mk_dest] using h
+    h ▸ ret r
+  | .tau y =>
+    have h : x = ITree.tau y := by
+      apply_fun MvQPF.Cofix.mk at h
+      simpa [MvQPF.Cofix.mk_dest] using h
+    h ▸ tau y
+  | .vis ⟨Ans, .up e, k⟩ =>
+    have h : x = ITree.vis e (fun ans => k (.up ans)) := by
+      apply_fun MvQPF.Cofix.mk at h
+      simpa [MvQPF.Cofix.mk_dest] using h
+    h ▸ vis e (fun ans => k (.up ans))
--- lakefile.lean
+++ lakefile.lean
@@ -7,16 +7,9 @@ package "lean-boogie" where
 @[default_target]
 lean_lib LeanBoogie where
   -- ## for 4.12:
-  require qpf from git "https://github.com/alexkeizer/QpfTypes.git" @ "ccb042d5260e6af8f5e81fa33db83ebf0c4e093f" -- for Lean 4.12
+  require qpf from git "https://github.com/Kiiyya/QpfTypes.git" @ "67e99ac5339969f1fe220100c3c5f05ea37e20f9" -- https://github.com/alexkeizer/QpfTypes/pull/52
   require auto from git "https://github.com/leanprover-community/lean-auto.git" @ "680d6d58ce2bb65d15e5711d93111b2e5b22cb1a" -- 4.12
   require Duper from git "https://github.com/leanprover-community/duper.git" @ "25c3ea88da2505158998eea07f40b07c0cdfe5ba"
-
-  -- ## for 4.11 (lean-auto only supports 4.11 for now):
-  -- require auto from git "https://github.com/leanprover-community/lean-auto.git" @ "60e546ca7a9d40d508e58847a9d0630406835178" -- 4.11, breaks duper, but parses z3 output.
-  -- -- require Duper from git "https://github.com/leanprover-community/duper.git" @ "v0.0.17"
-  -- -- require auto from git "https://github.com/leanprover-community/lean-auto.git" @ "2b6ed7d9f86d558d94b8d9036a637395163c4fa6" -- 4.11, works with duper, but fails to parse z3 output.
-  -- require Qq from git "https://github.com/leanprover-community/quote4.git" @ "9d0bdd07bdfe53383567509348b1fe917fc08de4" -- 4.11
-  -- require aesop from git "https://github.com/leanprover-community/aesop.git" @ "deb279eb7be16848d0bc8387f80d6e41bcdbe738" -- 4.11
 
 -- @[default_target]
 -- lean_exe "lean-boogie" where
