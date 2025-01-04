b912a4b36b85afbd959855b7ab3c5c1a88709dd3
Aaron Tomb <aarotomb@amazon.com>
2025-01-03 16:35:30 -0800

    Make `lake build` work again

--- LeanBoogie.lean
+++ LeanBoogie.lean
@@ -1,4 +1,4 @@
 -- This module serves as the root of the `LeanBoogie` library.
 -- Import modules here that should be built as part of the library.
 
-import LeanBoogie.BoogieDsl
+import LeanBoogie.Dsl
